3b3fddac3d1598f4590bea7c167d874c44684b4f
deewyz <134692502+deewyz@users.noreply.github.com>
2025-05-19 18:37:08 +0200

    inserimento menu

---
+++ Assets/MenuManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    public GameObject pannelloOpzioni;
+
+    public void AvviaGioco()
+    {
+        SceneManager.LoadScene("SampleScene"); // sostituisci con il nome reale della scena
+    }
+
+    public void ApriOpzioni()
+    {
+        if (pannelloOpzioni != null)
+            pannelloOpzioni.SetActive(true);
+    }
+
+    public void ChiudiOpzioni()
+    {
+        if (pannelloOpzioni != null)
+            pannelloOpzioni.SetActive(false);
+    }
+
+    public void MostraCrediti()
+    {
+        Debug.Log("🧑‍💻 Realizzato da Paolo Paradiso, 2025");
+    }
+
+    public void EsciGioco()
+    {
+        Application.Quit();
+        Debug.Log("🏁 Hai chiuso il gioco");
+    }
+}

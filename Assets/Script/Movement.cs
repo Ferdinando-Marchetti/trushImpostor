ff43997d32be3775708d5430e28a791167b4b15a
deewyz <134692502+deewyz@users.noreply.github.com>
2025-05-23 17:44:04 +0200

    modifiche paolo

--- Assets/Script/Movement.cs
+++ Assets/Script/Movement.cs
@@ -1,25 +1,29 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    public float speed;
-    public float mouseSensitivity;
+    public float speed = 5f;
+    public float runMultiplier = 1.5f;
+    public float jumpForce = 5f;
+    public float gravity = -9.81f;
+    public float mouseSensitivity = 100f;
     public static bool inputBloccato = false;
 
+    private float xRotation = 0f;
+    private float yVelocity = 0f;
+    private float initY;
 
-    float initY;
-    float xRotation = 0f;
-
-    Camera cameraChild;
-    CharacterController controller;
+    private Camera cameraChild;
+    private CharacterController controller;
 
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
-        initY = transform.position.y;
+        Cursor.visible = false;
 
+        initY = transform.position.y;
         cameraChild = GetComponentInChildren<Camera>();
         controller = GetComponent<CharacterController>();
     }
@@ -28,29 +32,46 @@ public class Movement : MonoBehaviour
     {
         if (inputBloccato)
         {
-            // Blocca il mouse
             Cursor.lockState = CursorLockMode.None;
             Cursor.visible = true;
             return;
         }
         else
         {
-            // Ripristina controllo mouse
             Cursor.lockState = CursorLockMode.Locked;
             Cursor.visible = false;
         }
+
+        // Movimento orizzontale
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
+        // Corsa con Shift
+        float currentSpeed = Input.GetKey(KeyCode.LeftShift) ? speed * runMultiplier : speed;
+
         Vector3 move = transform.right * x + transform.forward * z;
-        controller.Move(move * speed * Time.deltaTime);
+        move *= currentSpeed;
 
-        Vector3 currentPosition = transform.position;
-        currentPosition.y = initY;
-        transform.position = currentPosition;
+        // Salto
+        if (controller.isGrounded)
+        {
+            yVelocity = -2f; // forza verso il basso per mantenere contatto con il suolo
 
+            if (Input.GetButtonDown("Jump"))
+            {
+                yVelocity = Mathf.Sqrt(jumpForce * -2f * gravity);
+            }
+        }
+        else
+        {
+            yVelocity += gravity * Time.deltaTime;
+        }
 
+        move.y = yVelocity;
 
+        controller.Move(move * Time.deltaTime);
+
+        // Rotazione della visuale (mouse look)
         float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
         float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
 

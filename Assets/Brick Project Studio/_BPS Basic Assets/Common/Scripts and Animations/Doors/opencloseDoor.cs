d5d4ca6fb842f0983d987f460ce48873f7f42516
luigipierro <146242520+luigipierro@users.noreply.github.com>
2025-07-12 21:28:55 +0200

    Ultime modifiche

--- Assets/Brick Project Studio/_BPS Basic Assets/Common/Scripts and Animations/Doors/opencloseDoor.cs
+++ Assets/Brick Project Studio/_BPS Basic Assets/Common/Scripts and Animations/Doors/opencloseDoor.cs
@@ -2,71 +2,74 @@
 using UnityEngine;
 using TMPro;
 
-
-
 namespace SojaExiles
 {
     public class opencloseDoor : MonoBehaviour
     {
         public Animator openandclose;
-        public TMP_Text interactionText;
-        public bool open = false;
+        public bool open;
+        public Transform Player;
         public float interactionDistance = 3f;
-        public KeyCode interactKey = KeyCode.E;
-        
+        public TMP_Text interactionText;
+        public Camera playerCamera;
 
-        private Camera playerCamera;
+        private static opencloseDoor doorCurrentlyLookedAt;
 
         void Start()
         {
-            playerCamera = Camera.main;
-
+            open = false;
             if (interactionText != null)
-                interactionText.text = ""; // nascondi il testo all'inizio
+                interactionText.enabled = false;
         }
 
         void Update()
         {
+            if (Player == null || playerCamera == null || interactionText == null)
+                return;
+
+            float dist = Vector3.Distance(Player.position, transform.position);
             Ray ray = new Ray(playerCamera.transform.position, playerCamera.transform.forward);
             RaycastHit hit;
 
+            bool isLookingAtThis = false;
+
             if (Physics.Raycast(ray, out hit, interactionDistance))
             {
-                if (hit.transform == transform)
-                {
-                    if (interactionText != null)
-                    {
-                        interactionText.text = open ? "PREMI E PER CHIUDERE" : "PREMI E PER APRIRE";
-                    }
+                opencloseDoor hitDoor = hit.collider.GetComponentInParent<opencloseDoor>();
 
-                    if (Input.GetKeyDown(interactKey))
-                    {
-                        if (!open)
-                            StartCoroutine(opening());
-                        else
-                            StartCoroutine(closing());
-                    }
-                }
-                else
+                if (hitDoor == this && dist < interactionDistance)
                 {
-                    HideText();
+                    isLookingAtThis = true;
+                    doorCurrentlyLookedAt = this;
+                }
+            }
+
+            // Mostra il messaggio SOLO se stai guardando questa porta
+            if (doorCurrentlyLookedAt == this && isLookingAtThis)
+            {
+                interactionText.enabled = true;
+                interactionText.text = open ? "Premi E per chiudere" : "Premi E per aprire";
+
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    if (!open)
+                        StartCoroutine(opening());
+                    else
+                        StartCoroutine(closing());
                 }
             }
             else
             {
-                HideText();
+                if (interactionText.enabled && doorCurrentlyLookedAt == this)
+                {
+                    interactionText.enabled = false;
+                }
             }
-        }
-
-        void HideText()
-        {
-            if (interactionText != null)
-                interactionText.text = "";
         }
 
         IEnumerator opening()
         {
-            Debug.Log("Apertura porta");
+            Debug.Log("You are opening the door: " + gameObject.name);
             openandclose.Play("Opening");
             open = true;
             yield return new WaitForSeconds(0.5f);
@@ -74,7 +77,7 @@ namespace SojaExiles
 
         IEnumerator closing()
         {
-            Debug.Log("Chiusura porta");
+            Debug.Log("You are closing the door: " + gameObject.name);
             openandclose.Play("Closing");
             open = false;
             yield return new WaitForSeconds(0.5f);
